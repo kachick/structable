801eb5edbb886eb4f3ef3d9780980e40d894e295
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-08-10 13:27:37 +0900

    fix typo

--- lib/structable/classmethods.rb
+++ lib/structable/classmethods.rb
@@ -70,7 +70,7 @@ module Structable
     # @return [ClassMethods] new instance
     def load_pairs(pairs)
       unless pairs.respond_to?(:each_pair) and pairs.respond_to?(:keys)
-        raise TypeError, 'no pairs object'
+        raise TypeError, 'not pairs object'
       end
 
       raise ArgumentError, "different members" unless (pairs.keys - keys).empty?
