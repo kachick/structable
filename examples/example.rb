7a343d8d362af7d1328cbb4682c21c47f60cddbf
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-08-10 11:32:21 +0900

    modify style

---
+++ examples/example.rb
@@ -0,0 +1,25 @@
+#!/usr/local/bin/ruby -w
+
+$VERBOSE = true
+
+require_relative '../lib/structable'
+
+class Drink
+  include Structable
+
+  member :taste
+end
+
+class Tea < Drink
+  member :leaf
+  alias_member :type, :leaf
+end
+
+tea = Tea.new :bitter, :green
+p tea.members                          #=> [:taste, :leaf]
+p tea.members(true)                   #=> [:taste, :leaf, :type]
+p tea.member? :type                    #=> true
+tea.type = 'Special Flavor'
+p tea.taste                            #=> :bitter
+p tea.leaf                             #=> 'Special Flavor'
+p tea[0]                               #=> :bitter
