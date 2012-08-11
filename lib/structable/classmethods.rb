4a8c0949594782d18cbe3c3640f60625aff6df75
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-08-11 16:48:45 +0900

    add constructor(alias)

--- lib/structable/classmethods.rb
+++ lib/structable/classmethods.rb
@@ -68,7 +68,7 @@ module Structable
     
     # @param [#each_pair, #keys] pairs ex: Hash, Struct
     # @return [ClassMethods] new instance
-    def load_pairs(pairs)
+    def for_pairs(pairs)
       unless pairs.respond_to?(:each_pair) and pairs.respond_to?(:keys)
         raise TypeError, 'not pairs object'
       end
@@ -81,6 +81,8 @@ module Structable
         end
       }
     end
+    
+    alias_method :load_pairs, :for_pairs
 
     # @endgroup
     
